30b86f35c2290c8555bbac199d099bc7b9312a99
Raine-Jardim <raine.jardim@sptech.school>
2024-09-05 10:21:38 -0300

    Colocando verificações de campos vazios/email valido na página de login

--- Web Application/public/js/login.js
+++ Web Application/public/js/login.js
@@ -1,34 +1,91 @@
-function login() {
+let tudoCertoEmail = false;
+let tudoCertoSenha = false;
+
+function verificarEmail() {
 
     const email = input_email.value;
+
+    if (email == "") {
+
+        spanEmail.style.color = "red";
+        input_email.style.borderColor = "red";
+        tudoCertoEmail = false;
+
+    } else if (email.indexOf("@") < 0 || email.indexOf(".") < email.indexOf("@") || email.indexOf(".") == email.length - 1) {
+
+        alert("Email inválido!");
+        spanEmail.style.color = "red";
+        input_email.style.borderColor = "red";
+        tudoCertoEmail = false;
+
+    } else {
+
+        tudoCertoEmail = true;
+        spanEmail.style.color = "black";
+        input_email.style.borderColor = "#7F00FF";
+
+    }
+
+    verificarSenha();
+
+}
+
+function verificarSenha() {
+
     const senha = input_senha.value;
 
-    fetch('usuarios/autenticar', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            emailServer: email,
-            senhaServer: senha
-        }),
-    })
-        .then(function (resposta) {
-            if (resposta.ok) {
-                const mensagem = "Login realizado com sucesso..."
-                habilitarMensagem(mensagem);
-                setTimeout(function () {
-                   ocultarMensagem();
-                }, 3000)
-            } else {
-                throw 'Houve um erro ao realizar o login.'
-            }
+    if (senha == "") {
+
+        spanSenha.style.color = "red";
+        input_senha.style.borderColor = "red";
+        tudoCertoSenha = false;
+
+    } else {
+
+        tudoCertoSenha = true;
+        spanSenha.style.color = "black";
+        input_senha.style.borderColor = "#7F00FF";
+
+    }
+
+    login();
+
+}
+
+function login() {
+
+    if (tudoCertoEmail && tudoCertoSenha) {
+
+        const email = input_email.value;
+        const senha = input_senha.value;
+
+        fetch('usuarios/autenticar', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                emailServer: email,
+                senhaServer: senha
+            }),
         })
-        .catch(function (resposta) {
-            console.log(`#ERRO: ${resposta}`);
-            habilitarMensagem(resposta);
-        });
-    return false;
+            .then(function (resposta) {
+                if (resposta.ok) {
+                    const mensagem = "Login realizado com sucesso..."
+                    habilitarMensagem(mensagem);
+                    setTimeout(function () {
+                        ocultarMensagem();
+                    }, 3000)
+                } else {
+                    throw 'Houve um erro ao realizar o login.'
+                }
+            })
+            .catch(function (resposta) {
+                console.log(`#ERRO: ${resposta}`);
+                habilitarMensagem(resposta);
+            });
+        return false;
+    }
 }
 
 function habilitarMensagem(mensagem) {
