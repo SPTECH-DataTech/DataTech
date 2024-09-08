3bd73165968b818ef04c3c755a7e20d83ef9213f
TiagoPeSouzaa <tiago.souza@sptech.school>
2024-09-08 13:16:53 -0300

    Validações ok

--- Web Application/public/js/cadastro.js
+++ Web Application/public/js/cadastro.js
@@ -11,13 +11,20 @@ function validarToken(token_informado) {
     }
 }
 
+// Função para quando a mensagem estiver certa, sumir o texto.
+function removerErros(erroElemento) {
+    erroElemento.style.display = "none"; // Esconder a mensagem de erro
+}
+
 function cadastrar() {
-    let nome = input_nome.value;
-    let cpf = input_cpf.value;
-    let email = input_email.value;
-    let senha = input_senha.value;
-    let confirmacaoSenha = input_confirmacaoSenha.value;
-    let token_informado = input_token.value;
+
+
+    let nome = input_nome.value.trim();
+    let cpf = input_cpf.value.trim();
+    let email = input_email.value.trim();
+    let senha = input_senha.value.trim();
+    let confirmacaoSenha = input_confirmacaoSenha.value.trim();
+    let token_informado = input_token.value.trim();
 
     // ERROS DE VERIFICAÇÃO
     let erroNome = nome.length <= 1;
@@ -26,54 +33,105 @@ function cadastrar() {
     let erroSenhaNumero = true;
     let erroSenhaQtd = senha.length < 6;
     let erroConfirmacaoSenha = confirmacaoSenha != senha;
-    
-    // verificação de números 
-    for (let i = 0; i < senha.length; i++){
-        
-        if (!isNaN(senha[i])){
+    let erroEncontrado = false;
+
+
+    // verificação de números na senha
+    for (let i = 0; i < senha.length; i++) {
+
+        if (!isNaN(senha[i])) {
             erroSenhaNumero = false;
             break;
         }
     }
 
-    if (nome === "" || email === "" || senha === "" || token_informado === "" ){
-        alert ("Preencha todos os campos para continuar")
+    // Para limpar mensagem de erro.
+    input_nome.addEventListener("input", function () {
+        removerErros(erro_nome);
+    });
+
+    input_cpf.addEventListener("input", function () {
+        removerErros(erro_cpf);
+    });
+
+    input_email.addEventListener("input", function () {
+        removerErros(erro_email);
+    });
+
+    input_senha.addEventListener("input", function () {
+        removerErros(erro_senha);
+    });
+
+    input_confirmacaoSenha.addEventListener("input", function () {
+        removerErros(erro_confirmacaoSenha);
+    });
+
+    input_token.addEventListener("input", function () {
+        removerErros(erro_token);
+    });
+
+
+    if (nome == "" || email == "" || senha == "" || token_informado == "" || cpf == "" || confirmacaoSenha == "") {
+        alert("Preencha todos os campos para continuar")
         return false;
     }
-    else if (erroNome){
-        alert ("Nome inválido!")
-        return false;
+    else if (erroNome) {
+
+        erro_nome.innerText = "Nome inválido!";
+        erro_nome.style.display = "block";
+        input_nome.classList.add("input-erro");
+        erroEncontrado = true;
     }
-    else if (erroCPF){
-        alert ("Erro no CPF. Deve haver 14 dígitos.")
+    else if (erroCPF) {
+
+        erro_cpf.innerText = "Erro no CPF. Deve haver 14 dígitos.";
+        erro_cpf.style.display = "block";
+        input_cpf.classList.add("input-erro");
+        erroEncontrado = true;
     }
-    else if (erroEmail){
-        alert("Email inválidol Deve haver @ e .com!")
-        return false;
+    else if (erroEmail) {
+
+        erro_email.innerText = "Email inválido! Deve haver @ e .com!";
+        erro_email.style.display = "block";
+        input_email.classList.add("input-erro");
+        erroEncontrado = true;
     }
-    else if(erroSenhaQtd){
-        alert("Erro na senha. Deve haver mais de 5 digitos.")
-        return false;
+    else if (erroSenhaQtd) {
+
+        erro_senha.innerText = "Erro na senha. Deve haver mais de 5 dígitos.";
+        erro_senha.style.display = "block";
+        input_senha.classList.add("input-erro");
+        erroEncontrado = true;
     }
-    else if (erroSenhaNumero){
-        alert("Erro na senha, deve haver números.")
-        return false;
+    else if (erroSenhaNumero) {
+
+        erro_senha.innerText = "Erro na senha. Deve haver números.";
+        erro_senha.style.display = "block";
+        input_senha.classList.add("input-erro");
+        erroEncontrado = true;
     }
-    else if (erroConfirmacaoSenha){
-        alert("A confirmação de senha deve ser a mesma que a senha.")
-        return false;
+    else if (erroConfirmacaoSenha) {
+
+        erro_confirmacaoSenha.innerText = "A confirmação de senha deve ser a mesma que a senha.";
+        erro_confirmacaoSenha.style.display = "block";
+        input_confirmacaoSenha.classList.add("input-erro");
+        erroEncontrado = true;
     }
     // let empresa = validarToken(token_informado);
     //  if (!empresa) {
     //     return ;
     // }
+
+    if (erroEncontrado) {
+        return false;
+    }
+
     else {
         alert("Cadastro realizado com sucesso. Redirecionando para tela de Login!")
-     window.location.href = "./login.html"   
+        window.location.href = "./login.html"
     }
-    
-    
-    
+
+
     fetch('usuarios/cadastrar', {
         method: "POST",
         headers: {
@@ -135,6 +193,4 @@ function listarEmpresas() {
         .catch(function (erro) {
             console.log(`#ERRO: ${erro}`);
         });
-
-        
 }
