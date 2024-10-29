66f8c0ac94a41468c1d7e03e206dce0065bed79d
PajorVi <vinicius.marques@sptech.school>
2024-10-29 19:28:19 -0300

    alterando campo de empresa na insersao de usuario

--- Web Application/src/models/usuarioModel.js
+++ Web Application/src/models/usuarioModel.js
@@ -14,7 +14,7 @@ function cadastrar(nome, email, senha, fkEmpresa, cpf) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, fkEmpresa);
     
     var instrucaoSql = `
-        INSERT INTO usuario (nome, cpf, email, senha, fk_empresa) VALUES ('${nome}', '${cpf}', '${email}', '${senha}', '${fkEmpresa}');
+        INSERT INTO usuario (nome, cpf, email, senha, fkEmpresa) VALUES ('${nome}', '${cpf}', '${email}', '${senha}', '${fkEmpresa}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
