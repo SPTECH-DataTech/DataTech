f56bb9dfa04d5feb64a0ed09fdb7bbab0b840688
samuessouza <samuel.ssouza@sptech.school>
2024-11-10 18:20:37 -0300

    Implmentando tela adicionar fazendas e rota persistindo no BD

---
+++ Web Application/src/models/fazendaModel.js
@@ -0,0 +1,36 @@
+
+var database = require('../database/config');
+
+function listarEstados() {
+    console.log("ACESSEI O FAZENDA MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ");
+
+    var instrucaoSql = `SELECT * FROM estadosMunicipio;`;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function adicionarFazenda(nomeFazenda, tipoCafe, estadoFazenda, idEmpresa) {
+    console.log("ACESSEI O FAZENDA MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", nomeFazenda, tipoCafe, estadoFazenda, idEmpresa);
+
+    var instrucaoSql = `INSERT INTO fazenda (nome, tipoCafe, fkempresa, fkEstadosMunicipio) VALUES ( '${nomeFazenda}', '${tipoCafe}', '${estadoFazenda}', '${idEmpresa}');`;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function listarFazendas() {
+    console.log("ACESSEI O FAZENDA MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ");
+
+    var instrucaoSql = ` SELECT faz.*, em.estado AS estado
+    FROM fazenda faz
+    JOIN estadosMunicipio em ON faz.fkEstadosMunicipio = em.id;`;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+module.exports = {
+    listarEstados,
+    adicionarFazenda,
+    listarFazendas
+}
