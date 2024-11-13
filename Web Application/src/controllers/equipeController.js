0e40afcf3c295a5ee6491caf79d14441bbc6ec2b
lucasomsilva <lucas.msilva@sptech.school>
2024-11-13 18:09:38 -0300

    Adicionando e atualizando funcionários

--- Web Application/src/controllers/equipeController.js
+++ Web Application/src/controllers/equipeController.js
@@ -1,9 +1,18 @@
 var equipeModel = require("../models/equipeModel");
 
-function listarFuncionarios(req, res) {
-    var idEmpresa = req.params.idEmpresa;
+function carregarCargos(req, res) {
+    equipeModel.carregarCargos()
+        .then((resposta) => {
+            res.status(200).json(resposta);
+        })
+        .catch((erro) => {
+            console.error("Erro ao listar cargos: ", erro);
+            res.status(500).json({ erro: "Erro ao listar cargos" });
+        });
+}
 
-    equipeModel.listarFuncionarios(idEmpresa).then((resposta) => {
+function listarFuncionarios(req, res) {
+    equipeModel.listarFuncionarios().then((resposta) => {
         res.status(200).json(resposta);
     }).catch((erro) => {
         console.error("Erro ao listar funcionários: ", erro);
@@ -11,18 +20,104 @@ function listarFuncionarios(req, res) {
     });
 }
 
-function excluirFuncionarios(req, res) {
-    var idEmpresa = req.params.idEmpresa;
+function adicionar(req, res) {
+    var idCargo = req.body.idCargoServer;
+    var nome = req.body.nomeServer;
+    var cpf = req.body.cpfServer;
+    var email = req.body.emailServer;
+    var senha = req.body.senhaServer;
 
-    equipeModel.excluirFuncionarios(idEmpresa, nomeUsuario).then((resposta) => {
-        res.status(200).json(resposta);
-    }).catch((erro) => {
-        console.error("Erro ao excluir funcionários: ", erro);
-        res.status(500).json({ erro: "Erro ao excluir funcionários" });
-    });
+    if (idCargo == undefined) {
+        res.status(400).send("idCargo está undefined!");
+    } else if (nome == undefined) {
+        res.status(400).send("Seu nome está undefined!");
+    } else if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (senha == undefined) {
+        res.status(400).send("Sua senha está undefined!");
+    } else {
+
+        equipeModel.adicionar(idCargo, nome, cpf, email, senha)
+            .then(
+                function (resultado) {
+                    if (resultado.length > 0) {
+                        res.status(409).json({ erro: "Usuário já cadastrado!" });
+                    }
+                    res.status(200).json({
+                        resultado,
+                        message: "Cadastro realizado com sucesso!"
+                    });
+                })
+            .catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json({ erro: erro.sqlMessage });
+                }
+            )
+    }
+}
+
+function excluir(req, res) {
+    const idUsuario = req.body.idUsuarioServer;
+
+    equipeModel.excluir(idUsuario)
+        .then((resposta) => {
+            res.status(200).json({ message: "Funcionário excluído com sucesso!" });
+        })
+        .catch((erro) => {
+            console.error("Erro ao excluir funcionário: ", erro);
+            res.status(500).json({ erro: "Erro ao excluir funcionário" });
+        });
+}
+
+function editar(req, res) {
+    var idUsuario = req.body.idUsuario;
+    var nome = req.body.nomeServer;
+    var email = req.body.emailServer;
+    var idCargo = req.body.idCargoServer;
+
+    if (idUsuario == undefined) {
+        res.status(400).send("idUsuario está undefined!");
+    } else if (nome == undefined) {
+        res.status(400).send("Seu nome está undefined!");
+    } else if (email == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (idCargo == undefined) {
+        res.status(400).send("idCargo está undefined!");
+    } else {
+
+        equipeModel.editar(idUsuario, nome, email, idCargo)
+            .then(
+                function (resultado) {
+                    if (resultado.length > 0) {
+                        res.status(409).json({ erro: "Usuário já cadastrado!" });
+                    }
+                    res.status(200).json({
+                        resultado,
+                        message: "Cadastro realizado com sucesso!"
+                    });
+                })
+            .catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json({ erro: erro.sqlMessage });
+                }
+            )
+    }
 }
 
 module.exports = {
+    carregarCargos,
     listarFuncionarios,
-    excluirFuncionarios,
+    adicionar,
+    excluir,
+    editar,
 }
--- Web Application/src/routes/equipe.js
+++ Web Application/src/routes/equipe.js
@@ -3,12 +3,24 @@ var router = express.Router();
 
 var equipeController = require("../controllers/equipeController");
 
-router.get("/listarFuncionarios/:idEmpresa", function (req, res) {
+router.get("/carregarCargos", function (req, res) {
+  equipeController.carregarCargos(req, res);
+});
+
+router.get("/listarFuncionarios/1", function (req, res) {
   equipeController.listarFuncionarios(req, res);
 });
 
-router.delete("/excluirFuncionario/:idEmpresa/:nome", function (req, res) {
-  equipeController.excluirFuncionario(req, res);
+router.post("/adicionar", function (req, res) {
+  equipeController.adicionar(req, res);
+});
+
+router.delete("/excluir/:idUsuario", function (req, res) {
+  equipeController.excluir(req, res);
+});
+
+router.put("/editar/:idUsuario", function (req, res) {
+  equipeController.editar(req, res);
 });
 
 module.exports = router;
