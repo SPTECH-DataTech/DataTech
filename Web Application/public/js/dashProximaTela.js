56897a6d832f6b4ced12e92b824c55449ba9eaff
Raine-Jardim <raine.jardim@sptech.school>
2024-11-28 08:56:00 -0300

    merge

--- Web Application/public/js/dashProximaTela.js
+++ Web Application/public/js/dashProximaTela.js
@@ -150,3 +150,6 @@ function plotarGrafico(resposta) {
 //         });
 
 // }
+window.onload = function() {
+    document.getElementById("b_usuario").innerHTML = `${sessionStorage.NOME_USUARIO}`;
+};
